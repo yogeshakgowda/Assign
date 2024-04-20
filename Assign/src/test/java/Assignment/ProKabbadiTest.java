a9cefc12eab367fe663a439decd8aa15d8cb6c38
yogeshakgowda <yogesha.052k@gmail.com>
2024-04-20 11:16:39 +0530

    2

--- Assign/src/test/java/Assignment/ProKabbadiTest.java
+++ Assign/src/test/java/Assignment/ProKabbadiTest.java
@@ -15,7 +15,7 @@ public static void main(String[] args) throws InterruptedException {
      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
      driver.get("https://www.prokabaddi.com/standings");
      Thread.sleep(5000);
-     String s="Puneri Paltan";
+     String s=System.getProperty("Browser");
      Thread.sleep(2000);
     // driver.findElement(By.xpath("//button[@id='onetrust-reject-all-handler']")).click();
      Thread.sleep(2000);
